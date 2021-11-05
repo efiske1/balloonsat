77073504d1192f969f6622b59c90b7f2f30fd9a1
Kate Nechaeva <90990789+NotfatPrince@users.noreply.github.com>
2021-11-05 11:58:43 -0600

    Update sensors.py

--- sensors-code/sensors.py
+++ sensors-code/sensors.py
@@ -21,21 +21,20 @@ temperature_offset = -5
 if os.stat("/home/pi/EOSS-317/data_log.cvs").st_size == 0:
     file.write("Time,TMP,Env_temp,gas,humidity,pressure,altitude,acceleration_x,acceleration_y,acceleration_z,gyro_x,gyro_y,gyro_z,magnitometer_x,magnitometer_y,magnitometer_z,\n")
 
-while True:
-   
-    print("Temperature: %.2f degrees C" % tmp117.temperature)
-    print("Environmental Temperature : %.2f degrees C" % bme680.temperature)
-    print("Gas: %d ohm" % bme680.gas)
-    print("Humidity: %0.1f %%" % bme680.relative_humidity)
-    print("Pressure: %0.3f hPa" % bme680.pressure)
-    print("Altitude = %0.2f meters" % bme680.altitude)
-    print("Acceleration: X:%.2f, Y: %.2f, Z: %.2f m/s^2" % (icm.acceleration))
-    print("Gyro X:%.2f, Y: %.2f, Z: %.2f rads/s" % (icm.gyro))
-    print("Magnetometer X:%.2f, Y: %.2f, Z: %.2f uT" % (icm.magnetic))
+while True:   
+   # print("Temperature: %.2f degrees C" % tmp117.temperature)
+    #print("Environmental Temperature : %.2f degrees C" % bme680.temperature)
+   # print("Gas: %d ohm" % bme680.gas)
+   # print("Humidity: %0.1f %%" % bme680.relative_humidity)
+   # print("Pressure: %0.3f hPa" % bme680.pressure)
+   # print("Altitude = %0.2f meters" % bme680.altitude)
+    #print("Acceleration: X:%.2f, Y: %.2f, Z: %.2f m/s^2" % (icm.acceleration))
+   # print("Gyro X:%.2f, Y: %.2f, Z: %.2f rads/s" % (icm.gyro))
+   # print("Magnetometer X:%.2f, Y: %.2f, Z: %.2f uT" % (icm.magnetic))
 
     now= datetime.now()
     file.write(str(now)+","+str(tmp117.temperature)+","+str(bme680.temperature)+","+str(bme680.gas)+","+str(bme680.relative_humidity)+","+str(bme680.pressure)+","+str(bme680.altitude)+","+str(icm.acceleration)+","+str(icm.gyro)+","+str(icm.magnetic)+"\n")
     file.flush()
-time.sleep(5)
+time.sleep(1)
 file.close()
 
